57e9022ab3557b9a6887610694aef2215f42a895
Harshpatel0710 <harshpatel27905@gmail.com>
2025-02-14 00:56:47 +0530

    Add files via upload

---
+++ MyCaptain_CodingLoopsAssignment.py
@@ -0,0 +1,14 @@
+list = [-7,45,-18,-8,10,-63,33,100]
+print("List: ",list);
+print("Positive Numbers of List:")
+for i in list:
+    if i>0:
+        print(i);
+
+list1 = [90,-40,72,-37,27,2,-28,9]
+print("List1: ",list1);
+print("Positive Numbers of List1:")
+for i in list1:
+    if i>0:
+        print(i);
+
